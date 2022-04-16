42601f61cd90459b656b3d865ceeb4b3e15e8103
fawl_03 <razidias@gmail.com>
2022-04-16 17:23:04 -0300

    ATT 1.1

--- src/challenges.js
+++ src/challenges.js
@@ -1,14 +1,10 @@
 // Desafio 1
 function compareTrue(parametro1, parametro2) {
-  if(parametro1 === True && parametro2 === True){
+  if(parametro1 === true && parametro2 === true){
     
-    return True
+    return true
   
-  } else {
-    
-    return false
-
-  }
+  } else {return false}
 
 
 
